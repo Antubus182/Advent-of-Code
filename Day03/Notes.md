86dfb636d45f5665e7a6d033196b0047eb86c40e
Niels ten Thije <niels.ten.thije@zeton.nl>
2024-01-09 13:08:33 +0100

    Tried and got stuck on day 3

---
+++ Day03/Notes.md
@@ -0,0 +1,3 @@
+Split de complete input in een 2D array
+Loop over de entries tot je een getal vind
+vind het aantal digits (1,2,3)
--- Day03/main.go
+++ Day03/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Advent of code Day 3")
+	input := getInput("input.txt") //note that input is 140 lines of 140 characters
+	lines := strings.Split(input, "\n")
+	charlist := strings.Split(input, "")
+
+	for _, c := range charlist {
+		fmt.Println(c)
+	}
+
+	for _, l := range lines {
+		chars := strings.Split(l, "")
+		for _, ch := range chars {
+			fmt.Print([]byte(ch))
+		}
+		fmt.Println(len(chars))
+	}
+}
+
+func getInput(file string) string {
+	input, err := os.ReadFile(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return string(input)
 }
